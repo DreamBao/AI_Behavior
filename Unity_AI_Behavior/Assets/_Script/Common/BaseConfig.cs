03da9dcb6784b377f99f05780d7618f4ac29be4f
sida zeng <410830472@qq.com>
2017-11-29 17:26:59 +0800

    节点链接
    节点链接

--- Unity_AI_Behavior/Assets/_Script/Common/BaseConfig.cs
+++ Unity_AI_Behavior/Assets/_Script/Common/BaseConfig.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BaseConfig {
-    public int ID;
+    public uint ID;
     public string nodeName;
 
-
-    public static bool LoadData(string Path)
+    public static Dictionary<uint, BaseConfig> BaseConfigDic = new Dictionary<uint, BaseConfig>();
+    public static Dictionary<uint, BaseConfig> LoadData(string Path)
     {
+        BaseConfigDic.Clear();
         TextAsset txtAsset = Resources.Load<TextAsset>(Path);
-        Debug.Log("txt : " + txtAsset.text);
+        List<object> obj = MiniJSON.Json.Deserialize(txtAsset.text) as List<object>;
+        for(int i = 0;i < obj.Count; i++)
+        {
+            BaseConfig bc = new BaseConfig();
+            Dictionary<string, object> dic = obj[i] as Dictionary<string, object>;
+            bc.ID = uint.Parse(dic["id"].ToString());
+            bc.nodeName = dic["NodeName"].ToString();
 
-        return true;
+            if (!BaseConfigDic.ContainsKey(bc.ID))
+                BaseConfigDic.Add(bc.ID, bc);
+            else
+                BaseConfigDic[bc.ID] = bc;
+        }
+        Debug.Log(txtAsset.text);
+        return BaseConfigDic;
     }
 }
 
--- Unity_AI_Behavior/Assets/_Script/EditorWindow/AIBehaviorEditor.cs
+++ Unity_AI_Behavior/Assets/_Script/EditorWindow/AIBehaviorEditor.cs
@@ -1,5 +1,23 @@
-﻿using UnityEditor;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
+
+
+public enum MenuType
+{
+    Delete,
+    Line
+}
+
+public class EditorNodeInfo
+{
+    public uint ID;
+    public string NodeName;
+    public string Descript;
+    public Rect windowRect = new Rect(100, 100, 200, 200);
+    public EditorNodeInfo next;
+}
+
 
 public class AIBehaviorEditor : EditorWindow {
 
@@ -12,17 +30,18 @@ public class AIBehaviorEditor : EditorWindow {
     private Vector2 mScrollPos = new Vector2(0, 0);
 
     int windowCount = 2;
+    private Vector2 mousePos;
 
+    static Dictionary<uint, BaseConfig> baseConfigDic;
 
-    //Composite
-    string Paralle_Node = "ParallelNode";
-    string Selector_Node = "SelectorNode";
-    string Sequence_Node = "SequenceNode";
+    //Editor Node Index
+    List<EditorNodeInfo> editorNode = new List<EditorNodeInfo>();
+    EditorNodeInfo CurSelectNode;
+    EditorNodeInfo PreDrawNode;
 
-
-    bool s1 = false;
-    bool s2 = false;
-    bool s3 = false;
+    //on-off
+    bool DrawLineMode = false;
+    bool IsClickedOnNode = false;
 
     [@MenuItem("AI/AIEditor")]
     static void InitEditor()
@@ -34,7 +53,6 @@ public class AIBehaviorEditor : EditorWindow {
         Instance = editor;
     }
 
-
     static void InitBinding()
     {
         button = Resources.Load<Texture>(Constant.TEXTURE_PATH + "Button");
@@ -42,25 +60,50 @@ public class AIBehaviorEditor : EditorWindow {
 
     static void LoadConfigData()
     {
-        BaseConfig.LoadData("Config/BaseConfig");
+        baseConfigDic = BaseConfig.LoadData("Config/BaseConfig");
     }
 
-    public Rect windowRect0 = new Rect(20, 20, 120, 50);
-    public Rect windowRect = new Rect(100, 100, 200, 200);
-    public Rect windowRect2 = new Rect(100, 100, 200, 200);
-    public Rect windowRect3 = new Rect(100, 100, 200, 200);
     void OnGUI()
     {
+        Event e = Event.current;
+        mousePos = e.mousePosition;
+        
+        if(e.button == 1 && e.isMouse && !DrawLineMode)
+        {
+            RefreshSelectedWindow();
+            CreateWindowMenu(e);
+        }
+        else if(e.button == 0 && e.isMouse && DrawLineMode)
+        {
+            RefreshSelectedWindow();
+
+            if(IsClickedOnNode && PreDrawNode != null)
+            {
+                PreDrawNode.next = CurSelectNode;
+            }
+
+            DrawLineMode = false;
+            CurSelectNode = null;
+            PreDrawNode = null;
+        }
+        
+
         //----------------------------- MainView -----------------------
         mScrollPos = GUI.BeginScrollView(new Rect(0, 0, position.width - 240, position.height), this.mScrollPos, new Rect(0, 0, this.maxSize.x, this.maxSize.y));
         BeginWindows();
 
-        //GUI.Window(1, new Rect(0, 100, 100, 100), DoMyWindow, "mywindow");
         UpdateNodeGUI();
+ 
+        if(DrawLineMode && CurSelectNode != null)
+        {
+            //画线模式画出临时线
+            Rect endRect = new Rect(mousePos, new Vector2(10, 10));
+            DrawBezier(CurSelectNode.windowRect, endRect);
+            Repaint();
+        }
 
         EndWindows();
-        //GUI.Box(new Rect(0, 0, 100, 100), button);
-        //GUI.Box(new Rect(100, 0, 100, 100), "test");
+
         GUI.EndScrollView();
         //-----------------------------End MainView --------------------
         //-----------------------------Editor Button -------------------
@@ -68,56 +111,107 @@ public class AIBehaviorEditor : EditorWindow {
         int x = 0;
         int y = 0;
 
-
-
-        if (GUI.Button(new Rect(x, y, 240, 20), Paralle_Node))
-        {
-            s1 = true;
-        }
-        y += 20;
-        if (GUI.Button(new Rect(x, y, 240, 20), Selector_Node))
-        {
-            s2 = true;
-        }
-        y += 20;
-        if (GUI.Button(new Rect(x, y, 240, 20), Sequence_Node))
-        {
-            s3 = true;
-        }
-        y += 20;
-
+        if(baseConfigDic != null)
+            foreach(var config in baseConfigDic)
+            {
+                if(GUI.Button(new Rect(x, y, 240, 20), config.Value.nodeName))
+                {
+                    EditorNodeInfo eni = new EditorNodeInfo();
+                    eni.ID = config.Value.ID;
+                    eni.NodeName = config.Value.nodeName;
+                    eni.Descript = config.Value.nodeName;
+                    editorNode.Add(eni);
+                }
+                y += 20;
+            }
         GUI.EndGroup();
-        //windowRect0 = GUI.Window(0, new Rect(0, 1200, 120, 50), DoMyWindow, "My Window");
     }
 
-    void DoMyWindow1(int windowID)
-    {
-        GUILayout.Button("Hi");
-        GUI.DragWindow();
-    }
-
-    void DoMyWindow2(int windowID)
-    {
-        GUILayout.Button("Hi");
-        GUI.DragWindow();
-    }
-
-    void DoMyWindow3(int windowID)
+    void DoMyWindow(int windowID)
     {
         GUILayout.Button("Hi");
         GUI.DragWindow();
     }
 
     void UpdateNodeGUI()
-    {   for(int i = 0; i < 3; i++)
+    {
+        for (int i = 0; i < editorNode.Count; i++)
         {
-            if (i == 0 && s1)
-                windowRect = GUI.Window(i, windowRect, DoMyWindow1, Paralle_Node);
-            if (i == 1 && s2)
-                windowRect2 = GUI.Window(i, windowRect2, DoMyWindow2, Selector_Node);
-            if (i == 2 && s3)
-                windowRect3 = GUI.Window(i, windowRect3, DoMyWindow3, Sequence_Node);
+            editorNode[i].windowRect = GUI.Window(i, editorNode[i].windowRect, DoMyWindow, editorNode[i].NodeName);
+            if(editorNode[i].next != null)
+            {
+                DrawBezier(editorNode[i].windowRect, editorNode[i].next.windowRect);
+            }
         }
     }
- 
+
+    private void DrawBezier(Rect start, Rect end)
+    {
+        Vector3 startPos = new Vector3(start.x + start.width / 2, start.y + start.height / 2, 0);
+
+        Vector3 endPos = new Vector3(end.x + end.width / 2, end.y + end.height / 2, 0);
+        //Debug.Log("startPos : " + startPos + " end : " + endPos);
+        Vector3 startTan = startPos + Vector3.right * 50;
+        Vector3 endTan = endPos + Vector3.left * 50;
+        Color shadow = new Color(0, 0, 0, 0.7f);
+
+        for (int i = 0; i < 5; i++)
+        {
+            Handles.DrawBezier(startPos, endPos, startTan, endTan, shadow, null, 1 + (i * 2));
+        }
+
+        Handles.DrawBezier(startPos, endPos, startTan, endTan, Color.black, null, 1);
+    }
+
+
+    private void CreateWindowMenu(Event e)
+    {
+        GenericMenu menu = new GenericMenu();
+        if (IsClickedOnNode)
+        {
+            menu.AddItem(new GUIContent("Delete Node"), false, MenuCallback, MenuType.Delete);
+            menu.AddItem(new GUIContent("Draw Line"), false, MenuCallback, MenuType.Line);
+            IsClickedOnNode = false;
+        }
+        else
+        {
+            menu.AddItem(new GUIContent("Add Node"), false, MenuCallback, "Test");
+            //menu.AddItem(new GUIContent("Add Output"), false, MenuCallback, MenuType.Output);
+            //menu.AddItem(new GUIContent("Add Cale"), false, MenuCallback, MenuType.Cale);
+            //menu.AddItem(new GUIContent("Add Comp"), false, MenuCallback, MenuType.Comp);
+        }
+
+        e.Use();
+        menu.ShowAsContext();
+    }
+
+    private void MenuCallback(object type)
+    {
+        switch((MenuType)type)
+        {
+            case MenuType.Delete:
+                break;
+            case MenuType.Line:
+                DrawLineMode = true;
+                PreDrawNode = CurSelectNode;
+                break;
+        }
+    }
+
+    private void RefreshSelectedWindow()
+    {
+        for(int i = 0; i < editorNode.Count; i ++)
+        {
+            if(editorNode[i].windowRect.Contains(mousePos))
+            {
+                IsClickedOnNode = true;
+                CurSelectNode = editorNode[i];
+                break;
+            }
+            else
+            {
+                IsClickedOnNode = false;
+            }
+        }
+    }
 }
